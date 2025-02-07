3fbe04dc31960d4942d1de7a45b061cc706c96aa
prudhvi-1122 <prudhwi1998@gmail.com>
2025-02-07 22:46:35 +0530

    s3 bucket added

--- 10.remote_state/main.tf
+++ 10.remote_state/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "test" {}
 terraform {
   backend "s3" {
       bucket  =  "devrobop-s5"
-      key     = " test/terraform.tfstate"
-      region  = " us-east-1"
+      key     = "test/terraform.tfstate"
+      region  = "us-east-1"
   }
 }
